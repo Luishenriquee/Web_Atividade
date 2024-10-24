c30850e7d976e2f98f769ee491e69ffd200641e8
Luishenriquee <iluishenriquebr@gmail.com>
2024-10-23 23:18:22 -0300

    Adição da camada de acesso ao dados para o Beneficiário

---
+++ FI.AtividadeEntrevista/DML/Beneficiario.cs
@@ -0,0 +1,28 @@
+﻿namespace FI.AtividadeEntrevista.DML
+{
+    /// <summary>
+    /// Classe de cliente que representa o registo na tabela do Beneficiario do Banco de Dados
+    /// </summary>
+    public class Beneficiario
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// ClienteId
+        /// </summary>
+        public long ClienteId { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        public string CPF { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        public string Nome { get; set; }
+    }
+}
